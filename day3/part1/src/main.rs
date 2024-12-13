4e18339452505667a6ba79162c1799c133758cb9
andreitotoro <andreitotoro@gmail.com>
2024-12-14 00:22:35 +0200

    Finnish the day3 part1 puzzle

---
+++ day3/part1/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+use regex::Regex;
+
+struct MulData {
+    first_number: u128,
+    second_number: u128,
+}
+
+fn find_and_extract_muldata_from_valid_substrings(input: &str) -> Vec<MulData> {
+    let regex = Regex::new(r"mul\((\d+),\s*(\d+)\)").unwrap();
+
+    regex
+        .captures_iter(input)
+        .map(|cap| MulData {
+            first_number: cap[1].parse().unwrap(),
+            second_number: cap[2].parse().unwrap(),
+        })
+        .collect()
+}
+
+fn main() {
+    let text_string = fs::read_to_string("data.txt").unwrap();
+    let text: &str = &text_string;
+
+    let data = find_and_extract_muldata_from_valid_substrings(&text);
+
+    let mut total_product: u128 = 0;
+    for mul_data in data {
+        total_product += mul_data.first_number * mul_data.second_number;
+    }
+
+    println!("{total_product}")
+}
